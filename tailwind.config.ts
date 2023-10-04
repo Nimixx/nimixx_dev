d5350053a2d4561b03fc4633867f881a11a000e6
nimixx <nimixx.dev@proton.me>
2023-10-04 21:43:39 +0200

    Add base theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+   content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}'
+   ],
+   theme: {
+      extend: {
+         colors: {
+            primary: 'rgb(var(--color-primary) / <alpha-value>)',
+            primary_light: 'rgb(var(--color-primary-light) / <alpha-value>)',
+            primary_dark: 'rgb(var(--color-primary-dark) / <alpha-value>)',
+            primary_text: 'rgb(var(--color-primary-text) / <alpha-value>)',
+            secondary_text: 'rgb(var(--color-secondary-text) / <alpha-value>)',
+            accent_1: 'rgb(var(--color-accent) / <alpha-value>)',
+            accent_2: 'rgb(var(--color-accent2) / <alpha-value>)',
+            accent_3: 'rgb(var(--color-accent3) / <alpha-value>)',
+            succes: 'rgb(var(--color-success) / <alpha-value>)',
+            hero_ball: 'rgb(var(--color-hero-ball) / <alpha-value>)'
+         }
+      }
+   },
+   plugins: []
 }
 export default config
