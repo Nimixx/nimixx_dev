33b7940baed44701d061eacd9686aaf1c0174c8f
NiDEV <79410105+NiDEV-Tech@users.noreply.github.com>
2023-10-05 13:54:50 +0200

    intial shadcniui

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -4,10 +4,10 @@ import { SocialIcons } from '../ui/social-icon'
 
 export function Header() {
    return (
-      <header className="w-full border-b border-primary_light bg-primary_dark">
+      <header className="w-full border-b border-primary_light/50 bg-primary_dark">
          <section className="mx-auto flex h-16 w-3/4 items-center justify-between">
             <nav className="flex items-center gap-10">
-               <SiteName SiteName="Nimixx.dev" />
+               <SiteName SiteName="nimixx.dev" />
                <FullNav />
             </nav>
             <SocialIcons />
