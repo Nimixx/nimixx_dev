90b209dcf3771556a33503e46fafe32cdf3fc22d
Nimixx <ttadeasthelen@gmail.cz>
2023-10-11 17:14:09 +0200

    Add textarea

--- components/content/homepage/contact/ContactForm.tsx
+++ components/content/homepage/contact/ContactForm.tsx
@@ -1,47 +1,51 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
+type InputProps = {} & React.InputHTMLAttributes<HTMLInputElement>
+
+export function Input({ id, type, placeholder }: InputProps) {
+   return (
+      <input
+         id={id}
+         type={type}
+         placeholder={placeholder}
+         required
+         className="w-full appearance-none rounded-md border border-primary_light bg-primary_light/10 px-3 py-2 leading-tight text-secondary_text shadow focus:border-transparent focus:outline-none focus:ring-2 focus:ring-secondary"
+      />
+   )
+}
+
 export function ContactForm() {
    return (
       <div className="w-full max-w-xs">
          <form>
-            <div className='pb-4'>
+            <div className="pb-4">
                <label htmlFor="username" className="mb-2 block text-sm">
                   Username
                </label>
-               <input
-                  id="username"
-                  type="text"
-                  placeholder="Username"
-                  required
-                  className="w-full appearance-none rounded-md border border-primary_light bg-primary_light/10 px-3 py-2 leading-tight text-secondary_text shadow focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent"
-               />
+               <Input id="username" type="text" placeholder="Username" />
             </div>
-            <div className='pb-4'>
+            <div className="pb-4">
                <label htmlFor="email" className="mb-2 block text-sm">
                   Email
                </label>
-               <input
-                  id="email"
-                  type="text"
-                  placeholder="Your email"
-                  required
-                  className="w-full appearance-none rounded-md border border-primary_light bg-primary_light/10 px-3 py-2 leading-tight text-secondary_text shadow focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent"
-               />
+               <Input id="email" type="text" placeholder="Your email" />
             </div>
-            <div className='pb-4'>
+            <div className="pb-4">
+               <label htmlFor="subject" className="mb-2 block text-sm">
+                  Subject
+               </label>
+               <Input id="subject" type="text" placeholder="Subject" />
+            </div>
+            <div className="pb-4">
                <label htmlFor="message" className="mb-2 block text-sm">
                   Your message
                </label>
-               <input
-                  id="message"
-                  type="text"
-                  placeholder="Your message"
-                  required
-                  className="w-full appearance-none rounded-md border border-primary_light bg-primary_light/10 px-3 py-2 leading-tight text-secondary_text shadow focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent"
-               />
+               <textarea name="" id="message" className='w-full h-24 appearance-none rounded-md border border-primary_light bg-primary_light/10 px-3 py-2 leading-tight text-secondary_text shadow focus:border-transparent focus:outline-none focus:ring-2 focus:ring-secondary'></textarea>
             </div>
-            <Button type="submit" className="w-full">Submit form</Button>
+            <Button type="submit" className="w-full">
+               Submit form
+            </Button>
          </form>
       </div>
    )
